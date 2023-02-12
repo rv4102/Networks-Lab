faa29e93a4fb0168535a10f245c4f21a7aa8e46e
jatin0101 <jatin.gupta@kgpian.iitkgp.ac.in>
2023-02-12 17:07:24 +0530

    get request server side done

---
+++ assgn_4/client_test.c
@@ -0,0 +1,102 @@
+
+/*    THE CLIENT PROCESS */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// void recv_msg(char *buf, int sockfd, int n){
+//     for(int i=0; i < 1000; i++) buf[i] = '\0';
+// 	int k = 0;
+// 	while(1){
+// 		char buffer[n];
+// 		for(int i = 0;i<n;i++) buffer[i] = '\0';
+// 		int nn = recv(sockfd, buffer, n, 0);
+// 		int end = 0;
+// 		for(int i = 0;i<n;i++){
+// 			if(buffer[i]=='\0'){
+// 				end = 1; break;
+// 			}
+//             if(k && k%1000==0) buf = realloc(buf, (k/1000 + 1)*1000);
+// 			buf[k++] = buffer[i];
+// 		}
+// 		if(end) break;
+// 	}
+
+// }
+
+
+int recv_msg(char *buf, int sockfd, int n){
+    // for(int i=0; i < 10000; i++) buf[i] = '\0';
+    memset(buf, '\0', n);
+
+	int k = 0;
+	while(1){
+		char buffer[n];
+		for(int i = 0;i<n;i++) buffer[i] = '\0';
+		int nn = recv(sockfd, buffer, n, 0);
+		int end = 0;
+		for(int i = 0;i<nn;i++){
+			if(buffer[i]=='\0'){
+				end = 1; break;
+			}
+            if(k && k%n==0) buf = (char *)realloc(buf, (k/n + 1)*n);
+			buf[k++] = buffer[i];
+		}
+		if(end) break;
+	}
+    return k;
+
+}
+
+int main(int argc, char *argv[])
+{
+	int			sockfd ;
+	struct sockaddr_in	serv_addr;
+
+	int i;
+	// char buf[1000];
+
+	/* Opening a socket is exactly similar to the server process */
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Unable to create socket\n");
+		exit(0);
+	}
+
+	serv_addr.sin_family	= AF_INET;
+	inet_aton("127.0.0.1", &serv_addr.sin_addr);
+    // int port_no = atoi(argv[1]);    
+	serv_addr.sin_port	= htons(8000);
+
+	if ((connect(sockfd, (struct sockaddr *) &serv_addr,
+						sizeof(serv_addr))) < 0) {
+		perror("Unable to connect to server\n");
+		exit(0);
+	}
+
+	printf("Server connected\n");
+
+    char buf[] = "GET /Assgn-4.pdf HTTP/1.1\nHost:localhost:8000\nUser-Agent:Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0\nAccept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\nAccept:pdf\nAccept-Language:en-US,en;q=0.5\nConnection:close\n\n\n";
+    // printf("%d", int(strlen(buf));
+    send(sockfd, buf, strlen(buf)+1, 0);
+
+
+
+    char *buffer;
+    buffer = (char *)malloc(10);
+    int n = recv_msg(buffer, sockfd, 10);
+
+    // printf("%d", n);
+	printf("%s", buffer);		
+	close(sockfd);
+	// free(buf);
+	return 0;
+
+}
+
--- assgn_4/server.c
+++ assgn_4/server.c
@@ -17,17 +17,41 @@ process and a client process.
 #include <sys/socket.h> 
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 			/* THE SERVER PROCESS */
+
+int recv_msg(char *buf, int sockfd, int n){
+    // for(int i=0; i < 10000; i++) buf[i] = '\0';
+    memset(buf, '\0', n);
+
+	int k = 0;
+	while(1){
+		char buffer[n];
+		for(int i = 0;i<n;i++) buffer[i] = '\0';
+		int nn = recv(sockfd, buffer, n, 0);
+		int end = 0;
+		for(int i = 0;i<nn;i++){
+			if(buffer[i]=='\0'){
+				end = 1; break;
+			}
+            if(k && k%n==0) buf = (char *)realloc(buf, (k/n + 1)*n);
+			buf[k++] = buffer[i];
+		}
+		if(end) break;
+	}
+    return k;
+
+}
 
 int main()
 {
 	int			sockfd, newsockfd ; /* Socket descriptors */
-	int			clilen;
+	socklen_t			clilen;
 	struct sockaddr_in	cli_addr, serv_addr;
 
 	int i;
-	char buf[500000];		/* We will use this buffer for communication */
+	// char buf[500000];		/* We will use this buffer for communication */
 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("Cannot create socket\n");
@@ -36,7 +60,7 @@ int main()
 
 	serv_addr.sin_family		= AF_INET;
 	serv_addr.sin_addr.s_addr	= INADDR_ANY;
-	serv_addr.sin_port		= htons(20000);
+	serv_addr.sin_port		= htons(8000);
 
 	if (bind(sockfd, (struct sockaddr *) &serv_addr,
 					sizeof(serv_addr)) < 0) {
@@ -46,6 +70,14 @@ int main()
 	printf("Server running\n");
 
 	listen(sockfd, 5); 
+
+
+
+    FILE *file_log = fopen("AccessLog.txt", "a");
+    if(file_log == NULL){
+        printf("File not found\n");
+        return 0;
+    }
 	while (1) {
 
 		clilen = sizeof(cli_addr);
@@ -67,7 +99,112 @@ int main()
 					   the new socket.
 					*/
 
-			
+			char *buffer;
+            buffer = (char *)malloc(10);
+            int n = recv_msg(buffer, newsockfd, 10);
+            
+            // char new_time[100];
+            time_t current_time = time(NULL);
+            struct tm *time_struct = localtime(&current_time);
+            struct tm *current_times = time_struct;
+            time_struct->tm_mday += 3;
+            time_t new_time = mktime(time_struct);
+            char *time_string = ctime(&new_time);
+
+            char *token = strtok(buffer, " ");
+
+            printf("Command: %s\n", token);
+
+            char client_ip[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &cli_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+            fprintf(file_log, "%02d%02d%02d:%02d%02d%02d:%s:%d:",current_times->tm_mday, current_times->tm_mon+1, current_times->tm_year-100, current_times->tm_hour, current_times->tm_min, current_times->tm_sec, client_ip, ntohs(cli_addr.sin_port));
+
+            if(strcmp(token, "GET")==0){
+                char *path  = strtok(NULL, " ");
+                printf("path: %s\n", path);
+
+                fprintf(file_log, "%s:%s\n", token, path);
+                char *http_version = strtok(NULL, "\n");
+                printf("http_version: %s\n", http_version);
+                if(strcmp(http_version, "HTTP/1.1")){
+                    printf("Invalid HTTP version\n");
+                    close(newsockfd);
+                    exit(0);
+                }
+
+
+
+                FILE *file  = fopen(path+1, "r");
+                char response_[1000];
+                // FILE *file_write = fopen("file_write.pdf", "w");
+                // if(file_write == NULL){
+                //     printf("File not found\n");
+                //     close(newsockfd);
+                //     exit(0);
+                // }
+                if(file == NULL){
+                    printf("File not found\n");
+                    close(newsockfd);
+                    exit(0);
+                }
+                
+                memset(response_, '\0', 1000);
+                strcat(response_, "HTTP/1.1 200 OK\n");
+                send(newsockfd, response_, strlen(response_), 0);
+                
+                memset(response_, '\0', 1000);
+                strcat(response_, "Expires: ");
+                strcat(response_, time_string);
+                // strcat(response_, "\n");
+                send(newsockfd, response_, strlen(response_), 0);
+
+                memset(response_, '\0', 1000);
+                strcat(response_, "Cache-Control: no-store\n");
+                send(newsockfd, response_, strlen(response_), 0);
+
+                memset(response_, '\0', 1000);
+                strcat(response_, "Content-language: en-us\n");
+                send(newsockfd, response_, strlen(response_), 0);
+
+                memset(response_, '\0', 1000);
+                strcat(response_, "Content-length: ");
+                fseek(file,0,SEEK_END);
+                long fl = ftell(file);
+                char len_str[20];
+                sprintf(len_str, "%ld", fl);
+                strcat(response_, len_str);
+                strcat(response_, "\n");
+                send(newsockfd, response_, strlen(response_), 0);
+
+                memset(response_, '\0', 1000);
+                strcat(response_, "Content-type: pdf\n");
+                send(newsockfd, response_, strlen(response_), 0);
+
+                memset(response_, '\0', 1000);
+                strcat(response_, "Last modified: \n");
+                send(newsockfd, response_, strlen(response_), 0);
+
+                memset(response_, '\0', 1000);
+                strcat(response_, "\n");
+                send(newsockfd, response_, strlen(response_), 0);
+
+                memset(response_, '\0', 1000);
+                fseek(file, 0, SEEK_SET);
+                while(fgets(response_, 1000, file)!= NULL){
+                    // printf("%s", response_);
+                    send(newsockfd, response_, strlen(response_), 0);
+                }
+                memset(response_, '\0', 1000);
+                send(newsockfd, response_, strlen(response_)+1, 0);
+                fclose(file);
+                // fclose(file_write);
+
+
+            }else if(strcmp(token, "PUT")==0){
+                
+            }else{
+                printf("Invalid command\n");
+            }
 			close(newsockfd);
 			exit(0);
 		}
