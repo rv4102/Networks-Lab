cdd2d9992eb66def65a58d9b5e1177ed092567c5
jatin0101 <jatin.gupta@kgpian.iitkgp.ac.in>
2023-02-12 14:33:47 +0530

    started server side

---
+++ assgn_4/server.c
@@ -0,0 +1,80 @@
+/*
+			NETWORK PROGRAMMING WITH SOCKETS
+
+In this program we illustrate the use of Berkeley sockets for interprocess
+communication across the network. We show the communication between a server
+process and a client process.
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+			/* THE SERVER PROCESS */
+
+int main()
+{
+	int			sockfd, newsockfd ; /* Socket descriptors */
+	int			clilen;
+	struct sockaddr_in	cli_addr, serv_addr;
+
+	int i;
+	char buf[500000];		/* We will use this buffer for communication */
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Cannot create socket\n");
+		exit(0);
+	}
+
+	serv_addr.sin_family		= AF_INET;
+	serv_addr.sin_addr.s_addr	= INADDR_ANY;
+	serv_addr.sin_port		= htons(20000);
+
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+					sizeof(serv_addr)) < 0) {
+		printf("Unable to bind local address\n");
+		exit(0);
+	}
+	printf("Server running\n");
+
+	listen(sockfd, 5); 
+	while (1) {
+
+		clilen = sizeof(cli_addr);
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr,
+					&clilen) ;
+
+		if (newsockfd < 0) {
+			printf("Accept error\n");
+			exit(0);
+		}
+
+		if (fork() == 0) {
+
+			/* This child process will now communicate with the
+			   client through the send() and recv() system calls.
+			*/
+			close(sockfd);	/* Close the old socket since all
+					   communications will be through
+					   the new socket.
+					*/
+
+			
+			close(newsockfd);
+			exit(0);
+		}
+
+		close(newsockfd);
+	}
+	return 0;
+}
+			
+
