0fc971fffb6f44087e60b6d646c4952bfceef4ff
Rushil Venkateswar <rushilv14@gmail.com>
2023-02-13 12:16:15 +0530

    request generation successful

--- assgn_4/MyOwnBrowser.c
+++ assgn_4/MyOwnBrowser.c
@@ -7,6 +7,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <ctype.h>
+#include <time.h>
 
 #define BUFSIZE 50
 #define MAXRETSIZE 30045 // large sized buffer to accomodate dir command output
@@ -57,14 +58,15 @@ size_t receive_as_packet(char *command, int sockfd){
 }
 
 void split_command(char *msg, size_t len, char **split){
+    // printf("%zu\n", len);
     int i;
     size_t cmd_ptr=0, idx=0;
     for(i=0; i<len; i++){
-        if(msg[i] != ' '){
-            split[cmd_ptr][idx++] = msg[i];
+        if(msg[i] == '\n'){
+            split[cmd_ptr][idx++] = '\0';
         }
-        else if(msg[i] == '\n'){
-            continue;
+        else if(msg[i] != ' '){
+            split[cmd_ptr][idx++] = msg[i];
         }
         else{
             split[cmd_ptr][idx++] = '\0';
@@ -74,55 +76,138 @@ void split_command(char *msg, size_t len, char **split){
     }
 }
 
-void parse_ip(char *url, char *ip, int *port){
+void parse_ip(char *url, char *ip, char *port){
     size_t ptr=7, idx=0; // "http://" uses 7 characters
     while(isdigit(url[ptr]) || url[ptr] == '.'){
         ip[idx++] = url[ptr];
+        ptr++;
     }
+    ip[idx] = '\0';
 
     ptr=strlen(url)-1;
-    if( !isdigit(url[ptr]) )
+    if( !isdigit(url[ptr]) ){
+        strcpy(port, "80");
         return;
+    }
     
     while( isdigit(url[ptr]) ) ptr--;
     ptr++;
 
-    char port_str[10];
-    strcpy(port_str, url+ptr);
-
-    // string to integer
-    sscanf(port_str, "%d", port);
+    strcpy(port, url+ptr);
 }
 
-void frame_as_request(char **cmd_list, char *http_request){
+void get_content_type(char *url, char *type){
+    char *tok;
+    tok = strtok(url, ":"); // the first token has "http"
+    tok = strtok(NULL, ":"); // the second token has "//ip/path/to/file.xyz"
+
+    strcpy(tok, tok + strlen(tok)-3);
+
+    if( strcmp(tok, "html") == 0 ){
+        strcpy(type, "text/html");
+    }
+    else if( strcmp(tok, "pdf") == 0 ){
+        strcpy(type, "application/pdf");
+    }
+    else if( strcmp(tok, "jpg") == 0 ){
+        strcpy(type, "image/jpg");
+    }
+    else{
+        strcpy(type, "text/*");
+    }
+}
+
+// Adjust date by a number of days +/-
+void DatePlusDays( struct tm* date, int days ){
+    const time_t ONE_DAY = 24 * 60 * 60 ;
+
+    // Seconds since start of epoch
+    time_t date_seconds = mktime( date ) + (days * ONE_DAY) ;
+
+    // Update caller's date
+    // Use localtime because mktime converts to UTC so may change date
+    *date = *localtime( &date_seconds ) ; ;
+}
+
+void get_times(char *last_modified, char *current){
+    time_t t = time(NULL);
+    struct tm *time_struct = localtime(&t);
+    char *tmp = ctime(&t);
+    strcpy(current, tmp);
+    current[strlen(current)-1] = '\0';
+
+    DatePlusDays(time_struct, -2);
+    time_t new_time = mktime(time_struct);
+    tmp = ctime(&new_time);
+    strcpy(last_modified, tmp);
+    last_modified[strlen(last_modified)-1] = '\0';
+}
+
+void frame_request(char **cmd_list, char *time, char *accept_lang, char *ip, char *port,
+        char *modified_since, char *content_language, char *content_length, char *http_request) {
     size_t ptr_1=0, ptr_2=0;
-    http_request[0] = '\0';
-    strcat(http_request, "GET ");
+
+    char type[20];
+    // the type of content can be inferred from
+    get_content_type(cmd_list[1], type);
+    http_request[0]='\0';
+    strcat(http_request, cmd_list[0]);
+    // strcpy(http_request, cmd_list[0]);
+    strcat(http_request, " ");
 
     // get the url
     ptr_1 = 7;
     while(cmd_list[1][ptr_1] != '/') ptr_1++;
-    while(cmd_list[1][ptr_1] != '\0'){
-        http_request[ptr_2++] = cmd_list[ptr_1++];
+    ptr_2 = strlen(http_request);
+    while(cmd_list[1][ptr_1] != ':' && cmd_list[1][ptr_1] != '\0') {
+        http_request[ptr_2++] = cmd_list[1][ptr_1];
+        ptr_1++;
     }
 
-    // HTTP/1.1
-    ptr_1 = 0;
-    strcat(http_request, " HTTP/1.1\n");
-    
+    strcat(http_request, " HTTP/1.1\r\n");
 
-    http_request[ptr_2++] = '\n';
+    strcat(http_request, "Host: ");
+    strcat(http_request, ip);
+    strcat(http_request, ": ");
+    strcat(http_request, port);
+    strcat(http_request, "\r\n");
 
-    // User-Agent
-    user_agent[]
+    strcat(http_request, "Connection: close\r\n");
+    strcat(http_request, "Date: ");
+    strcat(http_request, time);
+    strcat(http_request, "\r\n");
+    strcat(http_request, "Accept: ");
+    strcat(http_request, type);
+    strcat(http_request, "\r\n");
+    strcat(http_request, "Accept-Language: ");
+    strcat(http_request, accept_lang);
+    strcat(http_request, "\r\n");
+    strcat(http_request, "If-Modified-Since: ");
+    strcat(http_request, modified_since);
+    strcat(http_request, "\r\n");
+    strcat(http_request, "Content-language: ");
+    strcat(http_request, content_language);
+    strcat(http_request, "\r\n");
+    strcat(http_request, "Content-length: ");
+    strcat(http_request, content_length);
+    strcat(http_request, "\r\n");
+    strcat(http_request, "Content-type: ");
+    strcat(http_request, type);
+    strcat(http_request, "\r\n");
+
+    return;
+}
+
+void parse_response(){
+
 }
 
 int main(){
-    int sockfd;
+    int sockfd, port_;
     struct sockaddr_in serv_addr;
     size_t n=0;
-    char msg[MAXRETSIZE], **cmd_list, ip[16];
-    int port=80; // default is 80
+    char *msg, **cmd_list, ip[16], port[6], http_request[4096], current_time[40], last_modified_time[40];
+    msg = (char *)malloc(MAXRETSIZE * sizeof(char));
     cmd_list = (char **)malloc(sizeof(char *) * 3);
     for(int i=0; i<3; i++){
         cmd_list[i] = (char *)malloc(sizeof(char) * MAXRETSIZE);
@@ -134,17 +219,19 @@ int main(){
 	}
 
     while(1){
+        port_=80;
         printf("MyOwnBrowser> ");
         getline(&msg, &n, stdin);
         split_command(msg, strlen(msg), cmd_list);
-        if( strcmp(cmd_list[0], "GET") ){
+        if( strcmp(cmd_list[0], "GET") == 0 ){
             // parse the IP address
-            parse_ip(cmd_list[1], ip, &port);
+            parse_ip(cmd_list[1], ip, port);
+            sscanf(port, "%d", &port_);
 
             // connect to the server
             serv_addr.sin_family = AF_INET;
             inet_aton(ip, &serv_addr.sin_addr);
-            serv_addr.sin_port = htons(port);
+            serv_addr.sin_port = htons(port_);
 
             if( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0 ){
                 perror("connect() failed.\n");
@@ -152,13 +239,17 @@ int main(){
             }
 
             // create an http request
+            get_times(last_modified_time, current_time);
+            frame_request(cmd_list, current_time, "en-us", ip, port, last_modified_time, "en-us", "\0", http_request);
 
+            // send this as packets
+            
 
         }
-        else if( strcmp(cmd_list[0], "PUT") ){
+        else if( strcmp(cmd_list[0], "PUT") == 0 ){
 
         }
-        else if( strcmp(cmd_list[0], "QUIT") ){
+        else if( strcmp(cmd_list[0], "QUIT") == 0 ){
             printf("Quitting now.\nBye.\n");
             break;
         }
