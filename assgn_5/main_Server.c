434f8cd161de2a6b338649ffc5c41c67d186fcfe
Rushil Venkateswar <rushilv14@gmail.com>
2023-03-18 11:42:41 +0530

    edit

--- assgn_5/main_Server.c
+++ assgn_5/main_Server.c
@@ -1,63 +1,116 @@
+#include <stdio.h>
 #include "mysocket.h"
+#include <signal.h>
 
-int main(){
-    int			sockfd, newsockfd ; /* Socket descriptors */
-	socklen_t			clilen;
-	struct sockaddr_in	cli_addr, serv_addr;
+#define SOCK_MYTCP 1964
+const int BUF_S = 102400;
+
+void SIG_PIPE_handler(int signo)
+{
+	printf("SIGPIPE caught\n");
+}
+
+int main()
+{
+	signal(SIGPIPE, SIG_PIPE_handler);
+	int sockfd, newsockfd; /* Socket descriptors */
+	int clilen;
+	struct sockaddr_in cli_addr, serv_addr;
 
 	int i;
-	char buf[100];		/* We will use this buffer for communication */
-	
-	if ((sockfd = my_socket(AF_INET, SOCK_MyTCP, 0)) < 0) {
+	char buf[BUF_S]; /* We will use this buffer for communication */
+
+	if ((sockfd = my_socket(AF_INET, SOCK_MyTCP, 0)) < 0)
+	{
 		perror("Cannot create socket\n");
 		exit(0);
 	}
 
-	printf("Server running\n");
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(20000);
 
-	serv_addr.sin_family		= AF_INET;
-	serv_addr.sin_addr.s_addr	= INADDR_ANY;
-	serv_addr.sin_port		= htons(20000);
-
-	if (my_bind(sockfd, (struct sockaddr *) &serv_addr,
-					sizeof(serv_addr)) < 0) {
+	if (my_bind(sockfd, (struct sockaddr *)&serv_addr,	sizeof(serv_addr)) < 0){
 		perror("Unable to bind local address\n");
 		exit(0);
 	}
+	my_listen(sockfd, 5);
 
-    my_listen(sockfd, 1);
-    while (1) {
-		printf("\nWaiting for the client connection\n");
-		clilen = sizeof(cli_addr);
-		newsockfd = my_accept(sockfd, (struct sockaddr *) &cli_addr,
-					&clilen) ;
-
-		if (newsockfd < 0) {
-			perror("Accept error\n");
-			exit(0);
-		}
-
-		printf("Client connected\n");
-
-        char buff[100];
-        memset(buff, '\0', 100);
-        int n = my_recv(newsockfd, buff, 100, 0);
-        printf("Received: %s\nLength: %d\n", buff, n);
-		memset(buff, '\0', 100);
-		n = my_recv(newsockfd, buff, 100, 0);
-        printf("Received: %s\nLength: %d\n", buff, n);
-		memset(buff, '\0', 100);
-		n = my_recv(newsockfd, buff, 100, 0);
-        printf("Received: %s\nLength: %d\n", buff, n);
-		memset(buff, '\0', 100);
-		strcpy(buff, "hello from the server");
-		n = my_send(newsockfd, buff, strlen(buff), 0);
-		memset(buff, '\0', 100);
-		n = my_recv(newsockfd, buff, 100, 0);
-        printf("Received: %s\nLength: %d\n", buff, n);
-		
-		my_close(newsockfd);
+	clilen = sizeof(cli_addr);
+	newsockfd = my_accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen);
+	if (newsockfd < 0){
+		perror("Accept error\n");
+		exit(0);
 	}
-	return 0;
+	
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 1");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
 
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 11");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 111");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 1111");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 11111");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 111111");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 1111111");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	memset(buf, 0, BUF_S);
+	strcpy(buf, "Message from server 11111111");
+	my_send(newsockfd, buf, strlen(buf) + 1, 0);
+	
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	memset(buf, 0, BUF_S);
+	my_recv(newsockfd, buf, BUF_S, 0);
+	printf("%s\n", buf);
+
+	my_close(newsockfd);
+	return 0;
 }
